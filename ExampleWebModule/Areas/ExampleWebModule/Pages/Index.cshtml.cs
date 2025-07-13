5a647b8eec2fc8954e9bbbc6fb4391765d3b2420
= <andrewboudreau@gmail.com>
2025-07-12 19:21:54 -0500

    Add ExampleWebModule project and related files
    - Updated `SharedTools.sln` to include "ExampleWebModule".
    - Created new Razor page `Index.cshtml` with basic structure.
    - Added code-behind `Index.cshtml.cs` with `IndexModel` class.
    - Updated `_ViewImports.cshtml` for Razor namespaces.
    - Modified `_ViewStart.cshtml` to set layout for views.
    - Added `background.png` for web application background.
    - Updated `styles.css` for background image and styling.

---
+++ ExampleWebModule/Areas/ExampleWebModule/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExampleWebModule.Areas.ExampleWebModule.Pages;
+
+[Area("ExampleWebModule")]
+public class IndexModel : PageModel
+{
+    private readonly IExampleBlobStorage storage;
+
+    public IndexModel(IExampleBlobStorage storage)
+    {
+        this.storage = storage;
+    }
+
+    public async Task OnGet()
+    {
+        await storage.SaveAsync(new ExampleData
+        {
+            Id = Guid.CreateVersion7(),
+            Name = "Test Data"
+        });
+    }
+}
